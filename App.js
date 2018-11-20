18ac6c2032f0fbee703b16fcd04f2c824333adea
Robert Tyrees <robert.tyree@stinkstudios.com>
2018-11-20 15:22:10 +0000

    add bouncy-heart example

--- App.js
+++ App.js
@@ -2,91 +2,55 @@ import React, { Component } from "react";
 import {
   StyleSheet,
   View,
-  ScrollView,
-  Dimensions,
-  Animated
+  Animated,
+  TouchableWithoutFeedback
 } from "react-native";
 
-import Moment from "./moment";
-
-import drink1 from "./images/drink1.jpg";
-import drink2 from "./images/drink2.jpg";
-import drink3 from "./images/drink3.jpg";
-import drink4 from "./images/drink4.jpg";
-
-const { width } = Dimensions.get("window");
-const Images = [
-  { image: drink1, title: "Vodka Cran" },
-  { image: drink2, title: "Old Fashion" },
-  { image: drink3, title: "Mule" },
-  { image: drink4, title: "Strawberry Daiquiri" }
-];
-
-const getInterpolate = (animatedScroll, i) => {
-  const inputRange = [
-    i - 1 * width, // -1 * width // - 414
-    i * width, // 0 or width // 0 // When at width we do don't translate
-    (i + 1) * width // 1 * width // 828 // when we swipe past we will translate 150 left on prev picture
-  ];
-
-  const outputRange = i === 0 ? [0, 0, 150] : [-300, 0, 150];
-
-  return animatedScroll.interpolate({
-    inputRange,
-    outputRange,
-    extrapolate: "clamp"
-  });
-};
-
-const getSeparator = i => {
-  return (
-    <View key={i} style={[styles.separator, { left: (i - 1) * width - 2.5 }]} />
-  );
-};
-
+import Heart from "./heart";
 export default class App extends Component {
   state = {
-    animatedScroll: new Animated.Value(0),
-    scrollEnabled: true
+    liked: false,
+    scale: new Animated.Value(0),
+    animations: [
+      new Animated.Value(0),
+      new Animated.Value(0),
+      new Animated.Value(0),
+      new Animated.Value(0),
+      new Animated.Value(0),
+      new Animated.Value(0)
+    ]
   };
 
-  handleFocus = focused => {
+  triggerLike = () => {
     this.setState({
-      scrollEnabled: !focused
+      liked: !this.state.liked
+    });
+    Animated.spring(this.state.scale, {
+      toValue: 2,
+      friction: 3
+    }).start(() => {
+      this.state.scale.setValue(0);
     });
   };
 
   render() {
+    const bouncyHeart = this.state.scale.interpolate({
+      inputRange: [0, 1, 2],
+      outputRange: [1, 0.8, 1]
+    });
+    const heartButtonStyle = {
+      transform: [{ scale: bouncyHeart }]
+    };
+
     return (
       <View style={styles.container}>
-        <ScrollView
-          pagingEnabled
-          horizontal
-          scrollEnabled={this.state.scrollEnabled}
-          scrollEventThrottle={16}
-          onScroll={Animated.event([
-            { nativeEvent: { contentOffset: { x: this.state.animatedScroll } } }
-          ])}
-        >
-          {Images.map((image, i) => {
-            return (
-              <Moment
-                key={i}
-                {...image}
-                translateX={getInterpolate(
-                  this.state.animatedScroll,
-                  i,
-                  Images.length
-                )}
-                onFocus={this.handleFocus}
-                focused={!this.state.scrollEnabled}
-              />
-            );
-          })}
-          {Array.apply(null, { length: Images.length + 1 }).map((_, i) =>
-            getSeparator(i)
-          )}
-        </ScrollView>
+        <View>
+          <TouchableWithoutFeedback onPress={this.triggerLike}>
+            <Animated.View style={heartButtonStyle}>
+              <Heart filled={this.state.liked} />
+            </Animated.View>
+          </TouchableWithoutFeedback>
+        </View>
       </View>
     );
   }
@@ -95,13 +59,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#333"
-  },
-  separator: {
-    backgroundColor: "#000",
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    width: 5
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
--- moment.js
+++ moment.js
@@ -1,91 +1,56 @@
 import React, { Component } from "react";
 import {
-  View,
-  Text,
   StyleSheet,
+  View,
   Animated,
-  TouchableWithoutFeedback,
-  Dimensions
+  TouchableWithoutFeedback
 } from "react-native";
 
-const { width, height } = Dimensions.get("window");
+import Heart from "./heart";
 
-export default class Moment extends Component {
+export default class App extends Component {
   state = {
-    scale: new Animated.Value(1)
+    liked: false,
+    scale: new Animated.Value(0),
+    animations: [
+      new Animated.Value(0),
+      new Animated.Value(0),
+      new Animated.Value(0),
+      new Animated.Value(0),
+      new Animated.Value(0),
+      new Animated.Value(0)
+    ]
   };
 
-  componentWillMount() {
-    this.bgFadeInterpolate = this.state.scale.interpolate({
-      inputRange: [0.9, 1],
-      outputRange: ["rgba(0,0,0,.3)", "rgba(0,0,0,0)"]
+  triggerLike = () => {
+    this.setState({
+      liked: !this.state.liked
     });
-    this.textFade = this.state.scale.interpolate({
-      inputRange: [0.9, 1],
-      outputRange: [0, 1]
+    Animated.spring(this.state.scale, {
+      toValue: 2,
+      friction: 3
+    }).start(() => {
+      this.state.scale.setValue(0);
     });
-
-    this.calloutTranslate = this.state.scale.interpolate({
-      inputRange: [0.9, 1],
-      outputRange: [0, 150]
-    });
-  }
-
-  handlePress = () => {
-    if (this.props.focused) {
-      Animated.timing(this.state.scale, {
-        toValue: 1,
-        duration: 300
-      }).start(() => this.props.onFocus(false));
-      return;
-    }
-    Animated.timing(this.state.scale, {
-      toValue: 0.9,
-      duration: 300
-    }).start(() => this.props.onFocus(true));
   };
+
   render() {
-    const animatedStyle = {
-      transform: [
-        { translateX: this.props.translateX },
-        { scale: this.state.scale }
-      ]
-    };
-
-    const bgFadeStyle = {
-      backgroundColor: this.bgFadeInterpolate
-    };
-    const textFadeStyle = {
-      opacity: this.textFade
-    };
-    const calloutStyle = {
-      transform: [{ translateY: this.calloutTranslate }]
+    const bouncyHeart = this.state.scale.interpolate({
+      inputRange: [0, 1, 2],
+      outputRange: [1, 0.8, 1]
+    });
+    const heartButtonStyle = {
+      transform: [{ scale: bouncyHeart }]
     };
 
     return (
       <View style={styles.container}>
         <View>
-          <View style={styles.container}>
-            <Animated.Image
-              source={this.props.image}
-              style={[styles.image, animatedStyle]}
-              resizeMode="cover"
-            />
-          </View>
-          <TouchableWithoutFeedback onPress={this.handlePress}>
-            <Animated.View
-              style={[StyleSheet.absoluteFill, styles.center, bgFadeStyle]}
-            >
-              <Animated.View style={[styles.textWrap, textFadeStyle]}>
-                <Text style={styles.title}>{this.props.title}</Text>
-              </Animated.View>
+          <TouchableWithoutFeedback onPress={this.triggerLike}>
+            <Animated.View style={heartButtonStyle}>
+              <Heart filled={this.state.liked} />
             </Animated.View>
           </TouchableWithoutFeedback>
-          <Animated.View style={[styles.callout, calloutStyle]}>
-            <View>
-              <Text style={styles.title}>{this.props.title}</Text>
-            </View>
-          </Animated.View>
         </View>
       </View>
     );
@@ -94,35 +59,8 @@ export default class Moment extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    overflow: "hidden",
-    width: width,
-    height: height
-  },
-  image: {
     flex: 1,
-    width: null,
-    height: null
-  },
-  center: {
+    alignItems: "center",
     justifyContent: "center"
-  },
-  textWrap: {
-    backgroundColor: "rgba(0,0,0,.5)",
-    paddingVertical: 10
-  },
-  title: {
-    backgroundColor: "transparent",
-    fontSize: 30,
-    color: "#FFF",
-    textAlign: "center"
-  },
-  callout: {
-    height: 150,
-    backgroundColor: "rgba(0,0,0,.5)",
-    justifyContent: "center",
-    position: "absolute",
-    bottom: 0,
-    right: 0,
-    left: 0
   }
 });
